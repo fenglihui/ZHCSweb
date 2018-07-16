2ae0eb77d8a02f9ed4c1482221772b5216592055
e <912545770@qq.com>
2018-07-16 18:09:27 +0800

    201807161809

---
+++ formsubmit.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/11
+ * Time: 11:30
+ */
+$year=$_GET["year"];
+$month=$_GET["month"];
+echo $year;
+echo $month;
+?>
---
+++ js/select.js
@@ -0,0 +1,33 @@
+$.fn.selectDate = function(){
+			var minYear = 2012;
+			var maxYear = (new Date).getFullYear()-1;
+			var minManage = 1;
+			var maxManage = 50;
+			var yearSel = document.getElementById('year');
+			var monthSel = document.getElementById('month');
+			var manageSel = document.getElementById('manage');
+	
+			for(var y = maxYear;y >= minYear;y--){
+				var yearOpt = document.createElement('option');
+				yearOpt.value = y
+				yearOpt.innerHTML = y+'年'
+				yearSel.appendChild(yearOpt)
+			}
+			for(var n =2;n<=12;n++){
+				var monthOpt = document.createElement('option');
+                monthOpt.value = n;
+				if( n<10){
+					monthOpt.innerHTML = '0' + n + '月';
+				}
+				else{
+					monthOpt.innerHTML = n + '月';
+				}
+				monthSel.appendChild(monthOpt);
+			}
+			for(var m =minManage;m<=maxManage;m++){
+				var manageOpt = document.createElement('option');
+				manageOpt.value = m;
+				manageOpt.innerHTML = '蔬菜' + m + '号'
+				manageSel.appendChild(manageOpt);
+			}
+		}
